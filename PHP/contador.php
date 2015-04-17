10de320bc3752e13aedba60cb482baff66d570f4
bl0819 <bl0819@192.168.7.1>
2015-04-17 20:41:57 +0200

    iniciial

---
+++ PHP/contador.php
@@ -0,0 +1,16 @@
+<? 
+$archivo = "contador.txt"; 
+$contador = 0; 
+
+$fp = fopen($archivo,"r"); 
+$contador = fgets($fp, 26); 
+fclose($fp); 
+
+++$contador; 
+
+$fp = fopen($archivo,"w+"); 
+fwrite($fp, $contador, 26); 
+fclose($fp); 
+
+echo "Esta página ha sido visitada $contador veces"; 
+?>
